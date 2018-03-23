4ac1e4281f37d21fd48827acaa6661f494a6f379
Edan Hauon <t-edhauo@microsoft.com>
2018-03-23 15:09:15 +0300

    change online_api to online + create DataBase

--- online/online/urls.py
+++ online/online/urls.py
@@ -1,4 +1,4 @@
-"""online_api URL Configuration
+"""online URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- online/online/wsgi.py
+++ online/online/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for online_api project.
+WSGI config for online project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "online_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "online.settings")
 
 application = get_wsgi_application()
