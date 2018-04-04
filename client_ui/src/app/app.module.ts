9d76305e951360e68049a25da3a80b3f5d90dc1c
Yftah Livny <shachmatos@gmail.com>
2018-04-04 12:29:29 +0300

    v-8 client ui init

--- client_ui/src/app/app.module.ts
+++ client_ui/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { HomeInfoComponent } from './home-info/home-info.component';
+import { ChannelComponent } from './channel/channel.component';
+import {ChannelService} from "./channel.service";
 
+
+const myRoots: Routes = [{
+  path: '',
+  component: HomeComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    JumbotronComponent,
+    HomeInfoComponent,
+    ChannelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(myRoots)
   ],
-  providers: [],
+  providers: [
+    ChannelService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client_ui/src/app/channel.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ChannelService {
+
+  constructor() { }
+
+
+  public getChannels() {
+    return [
+      {
+        title: 'Channel 1',
+        repositories: [
+          {
+            name: 'Ch1 - Repo 1',
+            desc: 'Ch1 - Repo 1 - repository 1 in channel 1',
+            url: 'http://www.google.com/?q=repositroy 1'
+          },
+          {
+            name: 'Ch1 - Repo 2',
+            desc: 'Ch1 - Repo 2 - repository 2 in channel 1',
+            url: 'http://www.google.com/?q=repositroy 2'
+          },
+          {
+            name: 'Ch1 - Repo 3',
+            desc: 'Ch1 - Repo 3 - repository 3 in channel 1',
+            url: 'http://www.google.com/?q=repositroy 3'
+          },
+          {
+            name: 'Ch1 - Repo 4',
+            desc: 'Ch1 - Repo 4 - repository 4 in channel 1',
+            url: 'http://www.google.com/?q=repositroy 4'
+          },
+        ]
+      },
+      {
+        title: 'Channel 2',
+        repositories: [
+          {
+            name: 'Ch2 - Repo 1',
+            desc: 'Ch2 - Repo 1 - repository 1 in channel 2',
+            url: 'http://www.google.com/?q=repositroy 1'
+          },
+          {
+            name: 'Ch2 - Repo 2',
+            desc: 'Ch2 - Repo 2 - repository 2 in channel 2',
+            url: 'http://www.google.com/?q=repositroy 2'
+          },
+          {
+            name: 'Ch2 - Repo 3',
+            desc: 'Ch2 - Repo 3 - repository 3 in channel 2',
+            url: 'http://www.google.com/?q=repositroy 3'
+          },
+          {
+            name: 'Ch2 - Repo 4',
+            desc: 'Ch2 - Repo 4 - repository 4 in channel 2',
+            url: 'http://www.google.com/?q=repositroy 4'
+          },
+        ]
+      },
+    ];
+  }
+
+
+}
