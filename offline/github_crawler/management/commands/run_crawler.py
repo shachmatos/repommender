79d290e93b4b64bdd83924df72a7ea199038e8d0
Edan Hauon <t-edhauo@microsoft.com>
2018-05-18 16:33:42 +0300

    add user crawl for each repo + User model

--- offline/github_crawler/management/commands/run_crawler.py
+++ offline/github_crawler/management/commands/run_crawler.py
@@ -6,12 +6,13 @@ from math import floor
 from django.utils import dateparse
 from github import Github
 from github.Repository import Repository as githubRepository
+from github.NamedUser import NamedUser as githubNamedUser
 from tqdm import tqdm, trange
 from django.core.management import BaseCommand
 
 from offline.settings import GITHUB_CONFIG, BASE_DIR
 import os
-from offline_calculator.models import Topic, Repository
+from offline_calculator.models import Topic, Repository, User, UserRepositories
 from datetime import datetime, timedelta
 
 
@@ -32,50 +33,22 @@ def update_topics():
 def get_repositories(threshold, do_not_resume=False):
     g = Github(client_id=GITHUB_CONFIG['client_id'], client_secret=GITHUB_CONFIG['client_secret'])
 
-    # repo_ids = set()
-    # writer = None
-    # topics_writer = None
-    # repos_writer = None
     storage_path = BASE_DIR + '/storage/'
     scan_file_path = storage_path + 'scan.csv'
-    topics_scanned_file = storage_path + 'topics.csv'
-    repos_scanned_file = storage_path + 'repos.csv'
-    starttime = time.time()
-    # resumed = False
+    start_time = time.time()
     complete = False
-    result_name = storage_path + str(floor(starttime)) + '_repositories.csv'
     last_topic = ''
 
     if os.path.exists(scan_file_path):
         print('Scan already running')
         return
 
-    # if os.path.exists(storage_path + 'scan_incomplete.csv'):
     if os.path.exists(storage_path + 'incomplete'):
         if do_not_resume:
             os.remove(storage_path + 'incomplete')
-            # os.remove(storage_path + 'scan_incomplete.csv')
-            # repo_file = open(scan_file_path, 'w')
-            # topics_file = open(topics_scanned_file, 'w')
-            # repo_ids_file = open(repos_scanned_file, 'w')
         else:
             with open(storage_path + 'incomplete', 'r') as resume:
                 last_topic = resume.read()
-                # print(last_topic)
-                # exit(1)
-            # resumed = True
-            # repo_ids = read_scanned_ids(repos_scanned_file)
-            # os.rename(storage_path + 'scan_incomplete.csv', scan_file_path)
-            # repo_file = open(scan_file_path, 'a')
-            # topics_file = open(topics_scanned_file, 'a')
-            # repo_ids_file = open(repos_scanned_file, 'a')
-    else:
-        pass
-        # repo_file = open(scan_file_path, 'w')
-        # topics_file = open(topics_scanned_file, 'w')
-        # repo_ids_file = open(repos_scanned_file, 'w')
-    # print(1)
-    # exit(1)
 
     topics = Topic.objects.filter(**{'name__gte': last_topic}).all()
 
@@ -98,7 +71,7 @@ def get_repositories(threshold, do_not_resume=False):
                 limit = g.get_rate_limit().rate.remaining
                 t.set_postfix_str('limit: ' + str(limit))
                 while limit < 10:
-                    print('Wating for rate reset time...')
+                    print('Waiting for rate reset time...')
                     time.sleep(g.rate_limiting_resettime - time.time() + 10)
                     limit = g.get_rate_limit().rate.remaining
 
@@ -111,79 +84,33 @@ def get_repositories(threshold, do_not_resume=False):
                         'topics': repo.get_topics(),
                         'languages': repo.get_languages(),
                         'watchers_count': repo.watchers_count,
-                        'forks_count':repo.forks_count,
-                        'open_issues':repo.open_issues,
-                        'subscribers_count':repo.subscribers_count,
+                        'forks_count': repo.forks_count,
+                        'open_issues': repo.open_issues,
+                        'subscribers_count': repo.subscribers_count,
                         'pushed_at': dateparse.parse_datetime(str(repo.pushed_at)),
                         'updated_at': dateparse.parse_datetime(str(repo.updated_at)),
                         'image': repo.owner.avatar_url,
                         "description": repo.description
                     },
                     id=repo.id
-                    # size=repo.size,
-                    # name=repo.name,
-                    # url=repo.html_url,
-                    # topics=repo.get_topics(),
-                    # languages=repo.get_languages(),
-                    # watchers_count=repo.watchers_count,
-                    # forks_count=repo.forks_count,
-                    # open_issues=repo.open_issues,
-                    # subscribers_count=repo.subscribers_count,
-                    # pushed_at=dateparse.parse_datetime(str(repo.pushed_at)),
-                    # updated_at=dateparse.parse_datetime(str(repo.updated_at))
                 )
-                t.update()
-                # time.sleep(3)
-                # repo_dict = {
-                #     "id": repo.id,
-                #     "topics": repo.get_topics(),
-                #     "url": repo.html_url,
-                #     "name": repo.name,
-                #     "updated_at": repo.updated_at,
-                #     "pushed_at": repo.pushed_at,
-                #     "size": repo.size,
-                #     "watchers_count": repo.watchers_count,
-                #     "forks_count": repo.forks_count,
-                #     "open_issues": repo.open_issues,
-                #     "subscribers_count": repo.subscribers_count,
-                #     "languages": repo.get_languages()
-                # }
-                # if writer is None:
-                #     fields = list(repo_dict.keys())
-                #     writer = csv.DictWriter(repo_file, fields)
-                #     if not resumed:
-                #         writer.writeheader()
-                #
-                # if repos_writer is None:
-                #     repos_writer = csv.writer(repo_ids_file)
-                #
-                # if topics_writer is None:
-                #     topics_writer = csv.writer(topics_file)
 
-                # if repo.id not in repo_ids:
-                    # writer.writerow(repo_dict)
-                    # repo_ids.add(repo.id)
-                    # repos_writer.writerow([repo.id])
-                    # time.sleep(3)
-            # topics_writer.writerow(topic)
+                for contributor in repo.get_contributors():  # type: githubNamedUser
+                    if User.objects.filter(id=contributor.id).exists():
+                        UserRepositories.objects.update_or_create(user_id=contributor.id, repo_id=repo.id)
+                t.update()
+
         complete = True
     finally:
-        endtime = time.time()
-        print("Runtime: " + str(endtime - starttime) + 's')
-        # repo_file.close()
-        # topics_file.close()
-        # repo_ids_file.close()
+        end_time = time.time()
+        print("Runtime: " + str(end_time - start_time) + 's')
         if complete:
             if os.path.exists(storage_path + 'incomplete'):
                 os.remove(storage_path + 'incomplete')
-            # os.remove(topics_scanned_file)
-            # os.remove(repos_scanned_file)
         else:
             f = open(storage_path + 'incomplete', 'w')
             f.writelines(last_topic)
             f.close()
-            result_name = storage_path + 'scan_incomplete.csv'
-        # os.rename(scan_file_path, result_name)
 
 
 class Command(BaseCommand):
@@ -204,13 +131,6 @@ class Command(BaseCommand):
                 raise
         do_not_resume = False
         resume = ''
-        # if os.path.exists(BASE_DIR + '/storage/scan_incomplete.csv'):
-        #     while resume not in ['y', 'Y', 'n', 'N']:
-        #         resume = input('Previous scan detected. Resume? (y/n)')
-        #         if resume == 'y' or resume == 'Y':
-        #             do_not_resume = False
-        #         else:
-        #             do_not_resume = True
         if os.path.exists(BASE_DIR + '/storage/incomplete'):
             while resume not in ['y', 'Y', 'n', 'N']:
                 resume = input('Previous scan detected. Resume? (y/n)')
