04e50f385df1d1ddc341729b5f022458bdbbaea7
edhauon <edanhauon@gmail.com>
2018-06-01 11:53:35 +0300

    align online models to offline

--- online/computation_service/models.py
+++ online/computation_service/models.py
@@ -23,6 +23,8 @@ class Repository(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     size = models.PositiveIntegerField()
     name = models.CharField(max_length=255)
+    image = models.URLField(null=True)
+    description = models.TextField(null=True)
     url = models.URLField(max_length=255)
     topics = models.TextField()
     languages = models.TextField()
@@ -33,36 +35,32 @@ class Repository(models.Model):
     updated_at = models.DateTimeField()
     pushed_at = models.DateTimeField()
 
-    recommended = models.ManyToManyField(
-        'Repository',
-        through='Recommendation',
-        through_fields=('source','target')
-    )
-
 
 class Recommendation(models.Model):
     class Meta:
         db_table = "recommendations"
-        unique_together = (('source', 'target'),)
+        unique_together = (('user', 'source', 'target'),)
 
-    source = models.ForeignKey('Repository', models.CASCADE, 'source', db_index=True)
+    user = models.ForeignKey('User', models.CASCADE, db_index=True)
+    source = models.PositiveIntegerField()
     target = models.ForeignKey('Repository', models.CASCADE, 'target', db_index=True)
+    channel_type = models.CharField(max_length=255)
     score = models.FloatField(max_length=64)
 
 
 class User(models.Model):
     class Meta:
         db_table = "users"
+
     id = models.PositiveIntegerField(primary_key=True)
     login = models.CharField(unique=True, max_length=50)
     avatar_url = models.URLField()
-    preferred_languages = models.TextField(null=True)
     preferred_topics = models.TextField(null=True)
 
 
-class UserRepositories(models.Model):
+class UserRepository(models.Model):
     class Meta:
         db_table = "user_repositories"
-    user_id = models.ForeignKey('User', models.CASCADE, db_index=True)
-    repo_id = models.ForeignKey('Repository', models.CASCADE, db_index=True)
+    user = models.ForeignKey('User', models.CASCADE, db_index=True)
+    repo = models.ForeignKey('Repository', models.CASCADE, db_index=True)
     contributions = models.PositiveIntegerField(default=0)
